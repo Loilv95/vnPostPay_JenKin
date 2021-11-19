5d87ad169e4ed1887d724d905d7cd9632aa5f020
loilv <loilv@seatechit.com.vn>
2021-11-19 15:45:05 +0700

    loilv
    Loilv update project

---
+++ src/main/java/com/example/vnPostPay_JenKin/VnPostPayJenKinApplication.java
@@ -0,0 +1,13 @@
+package com.example.vnPostPay_JenKin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnPostPayJenKinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnPostPayJenKinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vnPostPay_JenKin/VnPostPayJenKinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vnPostPay_JenKin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VnPostPayJenKinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
