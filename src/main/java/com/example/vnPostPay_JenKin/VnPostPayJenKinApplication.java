7960aa0f2c1e78480329dbdaf81503a62c617d23
loilv <loilv@seatechit.com.vn>
2021-11-22 13:53:30 +0700

    Update VnPostPayJenKinApplication.java
    update

--- src/main/java/com/example/vnPostPay_JenKin/VnPostPayJenKinApplication.java
+++ src/main/java/com/example/vnPostPay_JenKin/VnPostPayJenKinApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VnPostPayJenKinApplication {
-
+	//loilv
 	public static void main(String[] args) {
 		SpringApplication.run(VnPostPayJenKinApplication.class, args);
 	}
